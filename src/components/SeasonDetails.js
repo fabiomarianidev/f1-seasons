4cd37b563b86cd27a38997fe91a3b782294176bc
fabiomarianidev <70094393+fabiomarianidev@users.noreply.github.com>
2021-01-04 21:45:22 +0100

    Fix problem on rendering before data is loaded

--- src/components/SeasonDetails.js
+++ src/components/SeasonDetails.js
@@ -4,26 +4,48 @@ import { selectSeason } from "../ducks/seasonSlice";
 
 const SeasonDetails = () => {
     const { season, drivers, constructors, isLoading, isError } = useSelector(selectSeason);
-    const winningDriver = drivers[0].Driver.givenName + " " + drivers[0].Driver.familyName;
-    const winningConstructor = constructors[0].Constructor.name;
+
+
+    const displayDetails = () => {
+        if (isLoading) {
+            return <p>Please wait..</p>
+        }
+
+        if (isError) {
+            return <p>There was an error</p>
+        }
+
+        if ( season === 0) {
+            return <p>No season selected</p>
+        }
+
+        const winningDriver = drivers[0].Driver.givenName + " " + drivers[0].Driver.familyName;
+        const winningConstructor = constructors[0].Constructor.name;
+
+        return(
+            <>
+                <header className="seasonDetails__header">
+                    <p>Results for the {season} season</p>
+                </header>
+                <div className="seasonDetails__mainContent">
+                    <div className="seasonDetails__winners">
+                        <div className="seasonDetails__card">
+                            <div className="seasonDetails__title">Winning driver</div>
+                            <div className="seasonDetails__name">{winningDriver}</div>
+                        </div>
+                        <div className="seasonDetails__card">
+                            <div className="seasonDetails__title">Winning team</div>
+                            <div className="seasonDetails__name">{winningConstructor}</div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
 
     return(
         <section className="seasonDetails has-shadow">
-            <header className="seasonDetails__header">
-                <p>Results for the {season} season</p>
-            </header>
-            <div className="seasonDetails__mainContent">
-                <div className="seasonDetails__winners">
-                    <div className="seasonDetails__card">
-                        <div className="seasonDetails__title">Winning driver</div>
-                        <div className="seasonDetails__name">{winningDriver}</div>
-                    </div>
-                    <div className="seasonDetails__card">
-                        <div className="seasonDetails__title">Winning team</div>
-                        <div className="seasonDetails__name">{winningConstructor}</div>
-                    </div>
-                </div>
-            </div>
+            {displayDetails()}
         </section>
     )
 }
